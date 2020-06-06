f22890ac4bb732d68cf0b92f40d9fd3cfd85ccb7
steve-naherny <64172067+steve-naherny@users.noreply.github.com>
2020-06-05 22:34:41 -0500

    Attempting to merge audio with video

--- app/src/main/java/com/example/testapp/ItemListActivity.java
+++ app/src/main/java/com/example/testapp/ItemListActivity.java
@@ -21,14 +21,131 @@ import com.example.testapp.dummy.DummyContent;
 
 import java.util.List;
 import com.example.testapp.InvisibleVideoRecorder;
-/**
- * An activity representing a list of Items. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link ItemDetailActivity} representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
- */
+
+
+
+
+
+
+import com.example.testapp.BackgroundAudioService;
+
+import android.content.ComponentName;
+import android.os.RemoteException;
+import android.support.v4.media.MediaBrowserCompat;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.widget.Button;
+/*
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int STATE_PAUSED = 0;
+    private static final int STATE_PLAYING = 1;
+
+    private int mCurrentState;
+
+    private MediaBrowserCompat mMediaBrowserCompat;
+    private MediaControllerCompat mMediaControllerCompat;
+
+    private Button mPlayPauseToggleButton;
+
+    private MediaBrowserCompat.ConnectionCallback mMediaBrowserCompatConnectionCallback = new MediaBrowserCompat.ConnectionCallback() {
+
+        @Override
+        public void onConnected() {
+            super.onConnected();
+            try {
+                mMediaControllerCompat = new MediaControllerCompat(MainActivity.this, mMediaBrowserCompat.getSessionToken());
+                mMediaControllerCompat.registerCallback(mMediaControllerCompatCallback);
+                setSupportMediaController(mMediaControllerCompat);
+                getSupportMediaController().getTransportControls().playFromMediaId(String.valueOf(R.raw.warner_tautz_off_broadway), null);
+
+            } catch( RemoteException e ) {
+
+            }
+        }
+    };
+
+    private MediaControllerCompat.Callback mMediaControllerCompatCallback = new MediaControllerCompat.Callback() {
+
+        @Override
+        public void onPlaybackStateChanged(PlaybackStateCompat state) {
+            super.onPlaybackStateChanged(state);
+            if( state == null ) {
+                return;
+            }
+
+            switch( state.getState() ) {
+                case PlaybackStateCompat.STATE_PLAYING: {
+                    mCurrentState = STATE_PLAYING;
+                    break;
+                }
+                case PlaybackStateCompat.STATE_PAUSED: {
+                    mCurrentState = STATE_PAUSED;
+                    break;
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPlayPauseToggleButton = (Button) findViewById(R.id.button);
+
+        mMediaBrowserCompat = new MediaBrowserCompat(this, new ComponentName(this, BackgroundAudioService.class),
+                mMediaBrowserCompatConnectionCallback, getIntent().getExtras());
+
+        mMediaBrowserCompat.connect();
+
+        mPlayPauseToggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if( mCurrentState == STATE_PAUSED ) {
+                    getSupportMediaController().getTransportControls().play();
+                    mCurrentState = STATE_PLAYING;
+                } else {
+                    if( getSupportMediaController().getPlaybackState().getState() == PlaybackStateCompat.STATE_PLAYING ) {
+                        getSupportMediaController().getTransportControls().pause();
+                    }
+
+                    mCurrentState = STATE_PAUSED;
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if( getSupportMediaController().getPlaybackState().getState() == PlaybackStateCompat.STATE_PLAYING ) {
+            getSupportMediaController().getTransportControls().pause();
+        }
+
+        mMediaBrowserCompat.disconnect();
+    }
+}
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class ItemListActivity extends AppCompatActivity {
 
     /**
